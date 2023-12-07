d3d3aae1e81511379897c3b7b020bb5d6e11190e
mattthughes <>
2023-12-07 08:56:13 +0000

    Rewrote functions to improve readability

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def welcome_message():
         username = input("What is your name?\n")
 
         if not username:
-            print("Please enter your name\n")
+            print("Please enter your name.\n")
         else:
             intro(username)
             break
@@ -25,36 +25,36 @@ def escaped_classroom(username):
     to pick one out of three options
     """
 
-    print("As you escape the class room you notice a small dog in the corridor")
-    print("Do you A go and pet the dog. B turn aroun or C throw your book at the dog")
+    print("As you escape the classroom you notice a small dog in the corridor.")
+    print("Do you A throw your book at the dog. B turn around or C pet the dog.")
 
     while True:
         try:
-            escaped_choice = input("Please type either A or B or C\n")
+            escaped_choice = input("Please type either A or B or C.\n").upper()
             if escaped_choice == "A":
                 print("You throw a science book at the dog causing an explosion")
-                print("killing the dog, well done you live to fight another day")
+                print("killing the dog, well done you live to fight another day.")
                 open_door()
                 break
             elif escaped_choice == "B":
                 print("You turn around and you spot an even bigger dog, before you can react")
-                print("the dog bites you try to escape but you feel yourself slowing losing control")
-                print("you faint and as you lay there you can feel yourself getting weaker as you slowly lose your ")
-                print("soul to the underworld ")
-                print("Please try again")
+                print("the dog bites you try to escape but you feel yourself slowing losing control,")
+                print("you faint and as you lay there you can feel yourself getting weaker. As you slowly lose your ")
+                print("soul to the underworld.")
+                print("Please try again.")
                 welcome_message()
                 break
             elif escaped_choice == "C":
                 print("\nYou pet the dog and the dog turns around and bites you,")
-                print("you have been lost to the underworld forever\n")
-                print("Please try again\n")
+                print("you have been lost to the underworld forever.\n")
+                print("Please try again.\n")
                 welcome_message()
                 break
             else:
-                print(f"Please use the correct format by typing either A or B or C {username}")
+                print(f"Please use the correct format by typing either A or B or C. {username}")
                 continue
         except ValueError as e:
-            print("Invalid format please try again")
+            print("Invalid format please try again.")
 
 
 def run_game(username):
@@ -63,7 +63,7 @@ def run_game(username):
     to read a senario and make a decision
     """
 
-    print("\nYou wake up in detention and you hear the principal in the background storming to your class room")
+    print("\nYou wake up in detention and you hear the principal in the background storming to your class room.")
     print("You have three choices do you A go back to sleep and pretend its all just a bad dream. B wait for the principal to arrive")
     print("or C do you hide in the cupboard away from the principal?")
 
@@ -71,8 +71,8 @@ def run_game(username):
         try:
             first_choice = input(f"\nPlease type either A or B or C pick carefully {username}\n").upper()
             if first_choice == "A":
-                print("\nYou go back to sleep and the principle rushes in and throws a slice of pizza at your head")
-                print("A massive fear of yours you faint and die please try again\n")
+                print("\nYou go back to sleep and the principal rushes in and throws a slice of pizza at your head. which is")
+                print("a massive fear of yours, you faint and die please try again\n")
                 welcome_message()
                 break
             elif first_choice == "B":
@@ -102,10 +102,10 @@ def view_instructions(username):
     """
 
     print("\nInstructions")
-    print("Into Your Mind is a text based action adventure game where your decisions really matter")
-    print("During the game you will recieve a situation where your answer matters you will select your options by either typing A or B or C")
-    print("Depending on the situation you find yourself in think carefully as the wrong answer could mean Game Over")
-    print("If you would like to leave the game early at any point you can type EXIT to leave the application")
+    print("\nInto Your Mind is a text based action adventure game where your decisions really matter.")
+    print("During the game you will recieve a situation where your answer matters, you will select your options by either typing A or B or C.")
+    print("Depending on the situation you find yourself in think carefully as the wrong answer could mean Game Over.")
+    print("If you would like to leave the game early at any point you can type EXIT to leave the application\n")
 
     while True:
         try:
@@ -114,7 +114,7 @@ def view_instructions(username):
                 run_game(username)
                 break
             elif start == "B":
-                welcome_message()
+                intro(username)
                 break
             else:
                 print("\nPlease enter the correct format by either typing A or B")
@@ -128,7 +128,7 @@ def intro(username):
     allowing them to view the instructions or start the game
     """
 
-    print(f"Welcome to Into Your Mind {username} if you would like to Start the game type A or if you would like to view the instructions type B")
+    print(f"\nWelcome to Into Your Mind {username} if you would like to Start the game type A or if you would like to view the instructions type B")
 
     while True:
         try:
