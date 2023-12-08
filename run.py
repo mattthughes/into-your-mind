4ca0fe73e77476aa5ae8d7a8de5c587005061018
mattthughes <>
2023-12-08 08:45:52 +0000

    Added loading elements function to control loading speed,adjusted formatting

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ def output_delay(text_msg):
         print(i, end="", flush=True)
         time.sleep(0.07)
 
+def loading_elements(loading):
+    for i in loading:
+        print(i, end="", flush=True)
+        time.sleep(0.1)
+
 def welcome_message():
     """
     This function will greet the user and allow them to 
@@ -39,14 +44,14 @@ def abandoned_bus(username):
 
     while True:
         try:
-            bus_choice = input("Please type either A or B\n").upper()
+            bus_choice = input("\nPlease type either A or B\n").upper()
             if bus_choice == "A":
-                output_delay("You go through the open bus and spot an exit, you quickly make a dash for the exit and escape the school.\n")
+                output_delay("\nYou go through the open bus and spot an exit, you quickly make a dash for the exit and escape the school.\n")
                 output_delay("Now to find the way home and fast.\n")
                 journey_home(username)
                 break
             elif bus_choice == "B":
-                output_delay("You went through the open bus and hid under the seats, suddenly all the chewing gum under\n")
+                output_delay("\nYou went through the open bus and hid under the seats, suddenly all the chewing gum under\n")
                 output_delay("the seats has grown to the size of the school bus. Before you can react the chewing gum monster smothers you,\n")
                 output_delay("takes your soul banishing you to the underworld forever.\n")
                 welcome_message()
@@ -73,12 +78,12 @@ def open_door(username):
         try:
             open_door_choice = input("\nPlease type either A or B or C\n").upper()
             if open_door_choice == "A":
-                output_delay("You went through the open door, and were attacked and then eaten by an alien, one of your\n")
+                output_delay("\nYou went through the open door, and were attacked and then eaten by an alien, one of your\n")
                 output_delay("biggest fears you die.\n")
                 welcome_message()
                 break
             elif open_door_choice == "B":
-                output_delay("You slammed the door shut and alerted the principal, he\n")
+                output_delay("\nYou slammed the door shut and alerted the principal, he\n")
                 output_delay("finds you and takes you to his office. The principal begins to laugh at you\n")
                 output_delay("you get up and try and escape but the door is locked the principal begins to laugh\n")
                 output_delay(f"even more and starts to repeat you are here forever {username} HAHAHAHAHAHA!\n")
@@ -86,7 +91,7 @@ def open_door(username):
                 welcome_message()
                 break
             elif open_door_choice == "C":
-                output_delay("You went through the open window leading you to the outside playground.\n")
+                output_delay("\nYou went through the open window leading you to the outside playground.\n")
                 output_delay("escaping the monster through the open door.\n")
                 output_delay(f"You escaped for now {username}\n")
                 abandoned_bus(username)
@@ -115,12 +120,12 @@ def escaped_classroom(username):
         try:
             escaped_choice = input("\nPlease type either A or B or C.\n").upper()
             if escaped_choice == "A":
-                output_delay("You throw a science book at the dog causing an explosion\n")
+                output_delay("\nYou throw a science book at the dog causing an explosion\n")
                 output_delay("killing the dog, well done you live to fight another day.\n")
                 open_door(username)
                 break
             elif escaped_choice == "B":
-                output_delay("You turn around and you spot an even bigger dog, before you can react\n")
+                output_delay("\nYou turn around and you spot an even bigger dog, before you can react\n")
                 output_delay("the dog bites you try to escape but you feel yourself slowing losing control,\n")
                 output_delay("you faint and as you lay there you can feel yourself getting weaker. As you slowly lose your\n")
                 output_delay("soul to the underworld.\n")
@@ -134,7 +139,7 @@ def escaped_classroom(username):
                 welcome_message()
                 break
             else:
-                print(f"Please use the correct format by typing either A or B or C. {username}")
+                print(f"Please use the correct format by typing either A or B or C. {username}\n")
                 continue
         except ValueError as e:
             print("Invalid format please try again.")
@@ -145,7 +150,7 @@ def run_game(username):
     This is the first section of the game this function will allow the user 
     to read a senario and make a decision
     """
-    output_delay("Starting game....\n")
+    loading_elements("Starting game....\n")
     text_msg = """
     You wake up in detention and you hear the principal in the background storming to your class room.
     You have three choices do you A go back to sleep and pretend its all just a bad dream. 
@@ -186,7 +191,7 @@ def view_instructions(username):
     teaching them how the game works and any key information
     they will need while playing the game
     """
-    output_delay("\nInstructions loading....\n")
+    loading_elements("\nInstructions loading....\n")
 
     text_msg = """
     Into Your Mind is a text based action adventure game where your decisions really matter.
